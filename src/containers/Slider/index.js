1639df264a3c2c4e53631059a5b2fe2d81f0a7c7
boysers <boyser2015@gmail.com>
2023-02-20 11:42:41 +0100

    fix: slider and util function date

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -11,47 +11,57 @@ const Slider = () => {
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    if (byDateDesc) {
+      setIndex((idx) => (idx < byDateDesc.length - 1 ? idx + 1 : 0));
+    }
+  };
+  const switchCard = (id) => {
+    setIndex(id);
   };
   useEffect(() => {
-    nextCard();
-  });
+    const timer = setInterval(() => {
+      nextCard();
+    }, 5000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [index, nextCard]);
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img
+            className="SlideCard__pictureEvent"
+            src={event.cover}
+            alt="forum"
+          />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+            <input
+              key={`${radioIdx + 1}`}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onClick={() => switchCard(radioIdx)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
